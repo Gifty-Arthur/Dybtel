9a09f92d17e24dbe2123f7bf4941c5e5796bef51
Gifty Arthur <giftyarthur031@gmail.com>
2025-10-06 00:49:21 +0000

    update

--- src/components/MoneyCard.tsx
+++ src/components/MoneyCard.tsx
@@ -8,7 +8,7 @@ const MoneyCard = ({ amount }: MoneyCardProps) => {
       <div className="w-8 h-8 rounded-full bg-[#1e3a4c] flex items-center justify-center mb-2 mt-2">
         <span className="text-white text-xl font-bold">₵</span>
       </div>
-      <div className="text-white text-lg font-semibold">{amount}</div>
+      <div className="">{amount}</div>
     </button>
   );
 };
